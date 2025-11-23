e187d5161a27859f2d1a8336c3aec7510f911f29
Vitaliy <156187002+Vit-Tirsopluta@users.noreply.github.com>
2025-11-23 19:01:43 +0200

    modified line 8. file lab10

--- lab10/lab10.java
+++ lab10/lab10.java
@@ -5,7 +5,7 @@ public class lab10{
     public static void main(String args[]){
         JFrame myFrame = new JFrame("Приховане вікно");
         myFrame.setBounds(50, 60, 400,300);
-        myFrame.setVisible(true);
+        myFrame.setVisible(false);
         System.out.println("Window's location is x = " + myFrame.getX() + ", location y = " + myFrame.getY());
         System.out.println("Window's title is " + myFrame.getTitle());
         System.out.println("Window's state is " + myFrame.getState());
